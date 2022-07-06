9b79dca3dfcb31c2227e0a8f552ed38b6863cbc3
Russell Matney <russell.matney@gmail.com>
2022-07-06 01:00:39 -0400

    feat: highlighting all 'active' cells mostly there
    Still need to fix the mad flickering. Debugging sesh to follow.

--- src/SimpleTile.gd
+++ src/SimpleTile.gd
@@ -3,18 +3,35 @@ extends Node2D
 onready var tm = $TileMap
 
 var detectors = []
+var to_reset = {}
 
 enum {NONE, BLUE_LIGHT, BLUE_DARK, RED_LIGHT, RED_DARK}
 
+func _pos_to_cell(pos: Vector2) -> Vector2:
+  return tm.world_to_map(pos)
+
 func _process(_delta):
+  var to_mark = {}
   for detector in detectors:
-    # var extents = detector.get_shape().get_extents()
-    # print("extents", extents)
+    var det_pos = detector.global_position
+    if "extents" in detector:
+      var first_cell = _pos_to_cell(detector.global_position - detector.extents)
+      var last_cell = _pos_to_cell(detector.global_position + detector.extents)
+      for x in range(first_cell.x, last_cell.x + 1):
+        for y in range(first_cell.y, last_cell.y + 1):
+          to_mark[Vector2(x, y)] = true
+    else:
+      var cell = _pos_to_cell(detector.global_position)
+      to_mark[cell] = true
 
-    var detector_pos:Vector2 = detector.global_position
-    var cell:Vector2 = tm.world_to_map(detector_pos)
+  for v in to_reset.keys():
+    if not to_mark.has(v):
+      tm.set_cellv(v, BLUE_LIGHT if fmod(v.x + v.y, 2) == 0 else BLUE_DARK)
+      to_reset.erase(v)
 
-    tm.set_cell(cell.x, cell.y, RED_LIGHT if fmod(cell.x + cell.y, 2) == 0 else RED_DARK)
+  for v in to_mark.keys():
+    tm.set_cellv(v, RED_LIGHT if fmod(v.x + v.y, 2) == 0 else RED_DARK)
+    to_reset[v] = true
 
 func add_active_body(body):
   detectors.append(body)
--- src/TileDetector.gd
+++ src/TileDetector.gd
@@ -1,26 +1,23 @@
 extends Area2D
 # tightly coupled to SimpleTile.gd
 
+onready var extents = null
+
+func _ready():
+  if $CollisionShape2D:
+    extents = $CollisionShape2D.shape.extents
+  else:
+    Notif.notif("tile detector ought to know it's own extents!")
 
 func _on_TileDetector_body_entered(body):
-  print("tile detector detects body entered!")
-  print(body)
-
   if body is TileMap:
-    print("is tilemap!")
-
     # maybe should add the parent?
     if body.get_parent().has_method("add_active_body"):
       body.get_parent().add_active_body(self)
 
 
 func _on_TileDetector_body_exited(body:Node):
-  print("tile detector detects body exited!")
-  print(body)
-
   if body is TileMap:
-    print("is tilemap!")
-
     # maybe should add the parent?
     if body.get_parent().has_method("remove_active_body"):
       body.get_parent().remove_active_body(self)
