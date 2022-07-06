f1e5d7b2d42690d820ca830cad6477b858889369
Russell Matney <russell.matney@gmail.com>
2022-07-06 09:01:13 -0400

    fix: prevent tile flicker
    This issue describes the precise bug we hit:
    https://github.com/godotengine/godot/issues/61220
    
    For now we just don't handle the body_exited signal, and with this
    change we never remove an added 'detector' from the simpleTile impl.
    We'll need to handle this when we get to character death, but we may end
    up with a different method by then anyway. It would be nice to let the
    character or tileDetector determine how to interact with the
    tilemap (i.e. what color to set the tile to), but it might be odd to
    leak that detail.... maybe some tilemap public functions the consumers
    can choose between?

--- src/SimpleTile.gd
+++ src/SimpleTile.gd
@@ -2,46 +2,54 @@ extends Node2D
 
 onready var tm = $TileMap
 
-var detectors = []
+# A dictionary by instance_id
+var detectors = {}
+
+# cells to reset
 var to_reset = {}
+# cells to mark active
 var to_mark = {}
 
 enum {NONE, BLUE_LIGHT, BLUE_DARK, RED_LIGHT, RED_DARK}
-
-func _pos_to_cell(pos: Vector2) -> Vector2:
-  return tm.world_to_map(pos)
 
 func _is_even(i: int) -> bool:
   return fmod(i, 2) == 0
 
 func _process(_delta):
-  print("detectors", detectors)
-  for detector in detectors:
+  for detector in detectors.values():
+    # TODO check if detector is still alive?
+    # TODO consider letting the detector/something else determine what to indicate on the map?
     if "extents" in detector:
-      var first_cell = _pos_to_cell(detector.global_position - detector.extents)
-      var last_cell = _pos_to_cell(detector.global_position + detector.extents)
+      var first_cell = tm.world_to_map(detector.global_position - detector.extents)
+      var last_cell = tm.world_to_map(detector.global_position + detector.extents)
       for x in range(first_cell.x, last_cell.x + 1):
         for y in range(first_cell.y, last_cell.y + 1):
           to_mark[Vector2(x, y)] = true
     else:
-      print("detector without extents")
-      var cell = _pos_to_cell(detector.global_position)
+      print("detector without extents!")
+      var cell = tm.world_to_map(detector.global_position)
       to_mark[cell] = true
 
   for v in to_reset.keys():
     if not to_mark.has(v):
       # reset this cell to blue
-      tm.set_cellv(v, BLUE_LIGHT if _is_even(v.x + v.y) else BLUE_DARK)
+      var curr_idx: int = tm.get_cellv(v)
+      var new_idx: int = BLUE_LIGHT if _is_even(v.x + v.y) else BLUE_DARK
+      if curr_idx != new_idx:
+        tm.set_cellv(v, new_idx)
       to_reset.erase(v)
 
   for v in to_mark.keys():
-    tm.set_cellv(v, RED_LIGHT if _is_even(v.x + v.y) else RED_DARK)
+    var curr_idx: int = tm.get_cellv(v)
+    var new_idx: int = RED_LIGHT if _is_even(v.x + v.y) else RED_DARK
+    if curr_idx != new_idx:
+      tm.set_cellv(v, new_idx)
     to_mark.erase(v)
     # store to be reset on the next pass (unless it is marked again)
     to_reset[v] = true
 
 func add_active_body(body):
-  detectors.append(body)
+  detectors[body.get_instance_id()] = body
 
 func remove_active_body(body):
-  detectors.erase(body)
+  detectors.erase(body.get_instance_id())
--- src/TileDetector.gd
+++ src/TileDetector.gd
@@ -10,16 +10,13 @@ func _ready():
     Notif.notif("tile detector ought to know it's own extents!")
 
 func _on_TileDetector_body_entered(body):
-  print("body entered")
   if body is TileMap:
-    # maybe should add the parent?
     if body.get_parent().has_method("add_active_body"):
       body.get_parent().add_active_body(self)
 
 
-func _on_TileDetector_body_exited(body:Node):
-  print("body exited")
-  if body is TileMap:
-    # maybe should add the parent?
-    if body.get_parent().has_method("remove_active_body"):
-      body.get_parent().remove_active_body(self)
+# area2ds fire body_exited when tilemap set_cell is called
+# func _on_TileDetector_body_exited(body:Node):
+#   if body is TileMap:
+#     if body.get_parent().has_method("remove_active_body"):
+#       body.get_parent().remove_active_body(self)
