c57f8723b1d6f04f63190f40b50c09dbdb95864b
Russell Matney <russell.matney@gmail.com>
2022-07-14 11:13:59 -0400

    feat: basic health bar

--- src/ui/CharStatus.gd
+++ src/ui/CharStatus.gd
@@ -1,43 +1,47 @@
 extends Control
 
 var char_name_label
-var health_label
 var lives_label
 var score_label
 var combo_label
 
 var character
 
+var health_bar
+var health_bar_initial_rect
+
 ### ready #####################################################################
 
 func _ready():
   char_name_label = find_node("CharName")
-  health_label = find_node("Health")
   lives_label = find_node("Lives")
   score_label = find_node("Score")
   combo_label = find_node("Combo")
 
-  hide_labels()
+  health_bar = find_node("HealthBar")
+  health_bar_initial_rect = health_bar.rect_size
+
+  hide()
 
 ### show/hide labels #####################################################################
 
-func hide_labels():
+func hide():
   char_name_label.visible = false
-  health_label.visible = false
+  health_bar.visible = false
   lives_label.visible = false
   score_label.visible = false
   combo_label.visible = false
 
-func show_all_labels():
+func show_all():
   char_name_label.visible = true
-  health_label.visible = true
+  health_bar.visible = true
   lives_label.visible = true
   # score_label.visible = true
   combo_label.visible = true
 
-func show_basic_labels():
+func show_basic():
   char_name_label.visible = true
-  health_label.visible = true
+  health_bar.visible = true
 
 ### set char #####################################################################
 
@@ -45,24 +49,30 @@ func set_char(ch):
   character = ch
   set_char_label(ch.char_name if ch.char_name else ch.name)
   set_health(ch.current_health, ch.total_health)
-  show_basic_labels()
+  show_basic()
   if ch.is_player:
     set_lives(ch.lives)
     # set_score(ch.score)
     set_combos(ch.score_combo_count)
-    show_all_labels()
+    show_all()
 
 func clear_char():
-  hide_labels()
+  hide()
   character = null
 
 ### update labels #####################################################################
 
 func set_char_label(name):
-  char_name_label.bbcode_text = "[right]" + name + "[/right]"
+  char_name_label.bbcode_text = name
 
 func set_health(current: int, total: int):
-  health_label.bbcode_text = "[right]Health: " + str(current) + "/" + str(total) + "[/right]"
+  var bar_size_x = int(health_bar_initial_rect.x * (total / 10.0))
+  health_bar.rect_size.x = bar_size_x
+  health_bar.rect_min_size.x = bar_size_x
+  # health_bar_initial_rect.x = bar_size_x
+  # health_bar.set_size(health_bar_initial_rect)
+  health_bar.max_value = total
+  health_bar.value = current
 
 func set_lives(current: int):
   lives_label.bbcode_text = "[right]Lives: " + str(current) + "[/right]"
